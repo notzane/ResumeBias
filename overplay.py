9d661f2a068388ae6db567d4b905ccda5c04b9d4
Kush Patel <kp2zf@virginia.edu>
2018-03-03 19:32:51 -0500

    overlay 1.0
    adding work

---
+++ overplay.py
@@ -0,0 +1,36 @@
+# Overlay function
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+
+def overlay(pdf, box, x, y, height, width):
+    output_file = PdfFileWriter()
+    input_file = PdfFileReader(pdf, "rb")
+    logo = PdfFileReader(box, "rb")
+
+    # add page 1 from input1 to output document, unchanged
+    scale = 1
+
+    newfile = input_file.getPage(0)
+    newpic = logo.getPage(0)
+    newpic.scaleTo(width, height)
+
+    #newfile.mergeRotatedScaledTranslatedPage(newpics, 0, scale, x, y, expand=False)
+    newfile.mergeTranslatedPage(newpic, x, y, expand=False)
+
+    output_file.addPage(newfile)
+
+    outputStream = (open("resume_unbiased.pdf", "wb"))
+    output_file.write(outputStream)
+    outputStream.close()
+
+
+original = "test.pdf"
+red = "bigredbox.pdf"
+
+# MAX COORDINATES   x_cord, y_cord = 575, 780
+x_cord, y_cord = 245, 710
+w, h = 126, 21
+
+overlay(original, red, x_cord, y_cord, h, w)
+
